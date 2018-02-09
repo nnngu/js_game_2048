9bc74f0d42b03705d7ee8cf5212197a9a67982b9
nnngu <654015592@qq.com>
2018-02-10 02:52:46 +0800

    提交代码

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# js_game_2048
-🎮 js实现2048小游戏。
+# 🎮 js实现2048小游戏。 🎮
+
+## 1、游戏简介
+
+2048是一款休闲益智类的数字叠加小游戏。
+
+## 2、游戏玩法
+
+在 4*4 的16宫格中，您可以选择上、下、左、右四个方向进行操作，数字会按方向移动，相邻的两个数字相同就会合并，组成更大的数字，每次移动或合并后会自动增加一个数字。
+
+当16宫格中没有空格子，且四个方向都无法操作时，游戏结束。 
+
+## 3、游戏目的
+
+目的是合并出 2048 这个数字，获得更高的分数。
+
+## 4、游戏截图
+
+![](https://raw.githubusercontent.com/nnngu/FigureBed/master/2018/2/9/Snip20180209_1.png)
+
+## 5、游戏实现原理
+
+(1)首先，把16宫格看成是矩阵的形式
+
+![](https://raw.githubusercontent.com/nnngu/FigureBed/master/2018/2/9/Snip20180209_2.png)
+
+(2)在html中给每个格子添加类名及属性，来记录每个格子的位置
+
+![](https://raw.githubusercontent.com/nnngu/FigureBed/master/2018/2/9/Snip20180209_4.png)
+
+注：类名`item`是每个格子的类名，`emptyItem`是空格子的类名，`nonEmptyItem`是非空格子的类名。
+
+(3)游戏开始时，随机生成两个数字，2或者4，出现在矩阵中任意位置
+   
+![](https://raw.githubusercontent.com/nnngu/FigureBed/master/2018/2/9/Snip20180209_5.png)
+
+这部分是通过类名`emptyItem`及`nonEmptyItem`来实现的。
+
+步骤：
+
+①. 随机生成一个数字2或者4
+
+②. 获取所有空元素（类名`emptyItem`）
+
+③. 随机选择一个空元素，将生成的数字填充到空元素中，并将类名`emptyItem`移除，添加类名`nonEmptyItem`，即非空元素
+
+④. 重复①、②、③步，再随机生成一个数字填充到随机的位置。
+
+
+
---
+++ main.js
@@ -0,0 +1,358 @@
+$(function () {
+    //是否产生新元素
+    var isNewRndItme = false;
+    var gameScore = 0;
+    //最高分
+    var maxScore = 0;
+
+    if (localStorage.maxScore) {
+        maxScore = localStorage.maxScore - 0;
+    } else {
+        maxScore = 0;
+    }
+
+    //游戏初始化
+    gameInit();
+
+    function refreshGame() {
+        var items = $('.gameBody .row .item');
+        for (var i = 0; i < items.length; i++) {
+            items.eq(i).html('').removeClass('nonEmptyItem').addClass('emptyItem');
+        }
+        gameScore = 0;
+        //分数清零
+        $('#gameScore').html(gameScore);
+        //随机生成两个新元素
+        newRndItem();
+        newRndItem();
+        //刷新颜色
+        refreshColor();
+        $('#gameOverModal').modal('hide');
+    }
+
+
+    function getSideItem(currentItem, direction) {
+        //当前元素的位置
+        var currentItemX = currentItem.attr('x') - 0;
+        var currentItemY = currentItem.attr('y') - 0;
+
+        //根据方向获取旁边元素的位置
+        switch (direction) {
+            case 'left':
+                var sideItemX = currentItemX;
+                var sideItemY = currentItemY - 1;
+                break;
+            case 'right':
+                var sideItemX = currentItemX;
+                var sideItemY = currentItemY + 1;
+                break;
+            case 'up':
+                var sideItemX = currentItemX - 1;
+                var sideItemY = currentItemY;
+                break;
+            case 'down':
+                var sideItemX = currentItemX + 1;
+                var sideItemY = currentItemY;
+                break;
+        }
+        //旁边元素
+        var sideItem = $('.gameBody .row .x' + sideItemX + 'y' + sideItemY);
+        return sideItem;
+    }
+
+
+    function itemMove(currentItem, direction) {
+
+        var sideItem = getSideItem(currentItem, direction);
+
+        if (sideItem.length == 0) {//当前元素在最边上
+            //不动
+
+        } else if (sideItem.html() == '') { //当前元素不在最后一个且左（右、上、下）侧元素是空元素
+            sideItem.html(currentItem.html()).removeClass('emptyItem').addClass('nonEmptyItem');
+            currentItem.html('').removeClass('nonEmptyItem').addClass('emptyItem');
+            itemMove(sideItem, direction);
+            isNewRndItme = true;
+
+        } else if (sideItem.html() != currentItem.html()) {//左（右、上、下）侧元素和当前元素内容不同
+            //不动
+
+        } else {//左（右、上、下）侧元素和当前元素内容相同
+            //向右合并
+            sideItem.html((sideItem.html() - 0) * 2);
+            currentItem.html('').removeClass('nonEmptyItem').addClass('emptyItem');
+            gameScore += (sideItem.text() - 0) * 10;
+            $('#gameScore').html(gameScore);
+            // itemMove(sideItem, direction);
+            maxScore = maxScore < gameScore ? gameScore : maxScore;
+            $('#maxScore').html(maxScore);
+            localStorage.maxScore = maxScore;
+            isNewRndItme = true;
+            return;
+        }
+    }
+
+
+    function move(direction) {
+        //获取所有非空元素
+        var nonEmptyItems = $('.gameBody .row .nonEmptyItem');
+        //如果按下的方向是左或上，则正向遍历非空元素
+        if (direction == 'left' || direction == 'up') {
+            for (var i = 0; i < nonEmptyItems.length; i++) {
+                var currentItem = nonEmptyItems.eq(i);
+                itemMove(currentItem, direction);
+            }
+        } else if (direction == 'right' || direction == 'down') {//如果按下的方向是右或下，则反向遍历非空元素
+            for (var i = nonEmptyItems.length - 1; i >= 0; i--) {
+                var currentItem = nonEmptyItems.eq(i);
+                itemMove(currentItem, direction);
+            }
+        }
+
+        //是否产生新元素
+        if (isNewRndItme) {
+            newRndItem();
+            refreshColor();
+        }
+    }
+
+    function isGameOver() {
+        //获取所有元素
+        var items = $('.gameBody .row .item');
+        //获取所有非空元素
+        var nonEmptyItems = $('.gameBody .row .nonEmptyItem');
+        if (items.length == nonEmptyItems.length) {//所有元素的个数 == 所有非空元素的个数  即没有空元素
+            //遍历所有非空元素
+            for (var i = 0; i < nonEmptyItems.length; i++) {
+                var currentItem = nonEmptyItems.eq(i);
+                if (getSideItem(currentItem, 'up').length != 0 && currentItem.html() == getSideItem(currentItem, 'up').html()) {
+                    //上边元素存在 且 当前元素中的内容等于上边元素中的内容
+                    return;
+                } else if (getSideItem(currentItem, 'down').length != 0 && currentItem.html() == getSideItem(currentItem, 'down').html()) {
+                    //下边元素存在 且 当前元素中的内容等于下边元素中的内容
+                    return;
+                } else if (getSideItem(currentItem, 'left').length != 0 && currentItem.html() == getSideItem(currentItem, 'left').html()) {
+                    //左边元素存在 且 当前元素中的内容等于左边元素中的内容
+                    return;
+                } else if (getSideItem(currentItem, 'right').length != 0 && currentItem.html() == getSideItem(currentItem, 'right').html()) {
+                    //右边元素存在 且 当前元素中的内容等于右边元素中的内容
+                    return;
+                }
+            }
+        } else {
+            return;
+        }
+        $('#gameOverModal').modal('show');
+    }
+
+    //游戏初始化
+    function gameInit() {
+        //初始化分数
+        $('#gameScore').html(gameScore);
+        //最大分值
+        $('#maxScore').html(maxScore);
+        //为刷新按钮绑定事件
+        $('.refreshBtn').click(refreshGame);
+        //随机生成两个新元素
+        newRndItem();
+        newRndItem();
+        //刷新颜色
+        refreshColor();
+    }
+
+    //随机生成新元素
+    function newRndItem() {
+        //随机生成新数字
+        var newRndArr = [2, 2, 4];
+        var newRndNum = newRndArr[getRandom(0, 2)];
+        console.log('newRndNum: ' + newRndNum);
+        //随机生成新数字的位置
+        var emptyItems = $('.gameBody .row .emptyItem');
+        var newRndSite = getRandom(0, emptyItems.length - 1);
+        emptyItems.eq(newRndSite).html(newRndNum).removeClass('emptyItem').addClass('nonEmptyItem');
+    }
+
+    //产生随机数，包括min、max
+    function getRandom(min, max) {
+        return min + Math.floor(Math.random() * (max - min + 1));
+    }
+
+    //刷新颜色
+    function refreshColor() {
+        var items = $('.gameBody .item');
+        for (var i = 0; i < items.length; i++) {
+            // console.log(items.eq(i).parent().index());
+            switch (items.eq(i).html()) {
+                case '':
+                    items.eq(i).css('background', '');
+                    break;
+                case '2':
+                    items.eq(i).css('background', 'rgb(250, 225, 188)');
+                    break;
+                case '4':
+                    items.eq(i).css('background', 'rgb(202, 240, 240)');
+                    break;
+                case '8':
+                    items.eq(i).css('background', 'rgb(117, 231, 193)');
+                    break;
+                case '16':
+                    items.eq(i).css('background', 'rgb(240, 132, 132)');
+                    break;
+                case '32':
+                    items.eq(i).css('background', 'rgb(181, 240, 181)');
+                    break;
+                case '64':
+                    items.eq(i).css('background', 'rgb(182, 210, 246)');
+                    break;
+                case '128':
+                    items.eq(i).css('background', 'rgb(255, 207, 126)');
+                    break;
+                case '256':
+                    items.eq(i).css('background', 'rgb(250, 216, 216)');
+                    break;
+                case '512':
+                    items.eq(i).css('background', 'rgb(124, 183, 231)');
+                    break;
+                case '1024':
+                    items.eq(i).css('background', 'rgb(225, 219, 215)');
+                    break;
+                case '2048':
+                    items.eq(i).css('background', 'rgb(221, 160, 221)');
+                    break;
+                case '4096':
+                    items.eq(i).css('background', 'rgb(250, 139, 176)');
+                    break;
+            }
+        }
+    }
+
+    // 电脑的方向键监听事件
+    $('body').keydown(function (e) {
+        switch (e.keyCode) {
+            case 37:
+                // left
+                console.log('left');
+                isNewRndItme = false;
+                move('left');
+                isGameOver();
+                break;
+            case 38:
+                // up
+                console.log('up');
+                isNewRndItme = false;
+                move('up');
+                isGameOver();
+                break;
+            case 39:
+                // right
+                console.log('right');
+                isNewRndItme = false;
+                move('right');
+                isGameOver();
+                break;
+            case 40:
+                // down
+                console.log('down');
+                isNewRndItme = false;
+                move('down');
+                isGameOver();
+                break;
+        }
+    });
+
+    // 手机屏幕划动触发
+    (function () {
+        mobilwmtouch(document.getElementById("gameBody"))
+        document.getElementById("gameBody").addEventListener('touright', function (e) {
+            e.preventDefault();
+            // alert("方向向右");
+            console.log('right');
+            isNewRndItme = false;
+            move('right');
+            isGameOver();
+
+        });
+        document.getElementById("gameBody").addEventListener('touleft', function (e) {
+            // alert("方向向左");
+            console.log('left');
+            isNewRndItme = false;
+            move('left');
+            isGameOver();
+
+        });
+        document.getElementById("gameBody").addEventListener('toudown', function (e) {
+            // alert("方向向下");
+            console.log('down');
+            isNewRndItme = false;
+            move('down');
+            isGameOver();
+        });
+        document.getElementById("gameBody").addEventListener('touup', function (e) {
+            // alert("方向向上");
+            console.log('up');
+            isNewRndItme = false;
+            move('up');
+            isGameOver();
+        });
+
+        function mobilwmtouch(obj) {
+            var stoux, stouy;
+            var etoux, etouy;
+            var xdire, ydire;
+            obj.addEventListener("touchstart", function (e) {
+                stoux = e.targetTouches[0].clientX;
+                stouy = e.targetTouches[0].clientY;
+                //console.log(stoux);
+            }, false);
+            obj.addEventListener("touchend", function (e) {
+                etoux = e.changedTouches[0].clientX;
+                etouy = e.changedTouches[0].clientY;
+                xdire = etoux - stoux;
+                ydire = etouy - stouy;
+                chazhi = Math.abs(xdire) - Math.abs(ydire);
+                //console.log(ydire);
+                if (xdire > 0 && chazhi > 0) {
+                    console.log("right");
+                    //alert(evenzc('touright',alerts));
+                    obj.dispatchEvent(evenzc('touright'));
+
+                } else if (ydire > 0 && chazhi < 0) {
+                    console.log("down");
+                    obj.dispatchEvent(evenzc('toudown'));
+                } else if (xdire < 0 && chazhi > 0) {
+                    console.log("left");
+                    obj.dispatchEvent(evenzc('touleft'));
+                } else if (ydire < 0 && chazhi < 0) {
+                    console.log("up");
+                    obj.dispatchEvent(evenzc('touup'));
+                }
+            }, false);
+
+            function evenzc(eve) {
+                if (typeof document.CustomEvent === 'function') {
+
+                    this.event = new document.CustomEvent(eve, {//自定义事件名称
+                        bubbles: false,//是否冒泡
+                        cancelable: false//是否可以停止捕获
+                    });
+                    if (!document["evetself" + eve]) {
+                        document["evetself" + eve] = this.event;
+                    }
+                } else if (typeof document.createEvent === 'function') {
+
+
+                    this.event = document.createEvent('HTMLEvents');
+                    this.event.initEvent(eve, false, false);
+                    if (!document["evetself" + eve]) {
+                        document["evetself" + eve] = this.event;
+                    }
+                } else {
+                    return false;
+                }
+
+                return document["evetself" + eve];
+
+            }
+        }
+    })();
+});
